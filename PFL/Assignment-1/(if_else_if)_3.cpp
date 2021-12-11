fec35a3781d82af13c724095826de9532429decb
abdurrehman0206 <68471828+abdurrehman0206@users.noreply.github.com>
2021-12-11 17:35:32 +0500

    PFL/Assignment-1/(if_else_if)_3.:

---
+++ PFL/Assignment-1/(if_else_if)_3.cpp
@@ -0,0 +1,40 @@
+//Program based on logic of app redirection based on age
+//Assuming customer base is under age of 61
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float age;
+    cout << "If you are 60+ please contact customer support rep!" << endl;
+    cout << "Enter Age :: ";
+    cin >> age;
+
+        if (age <=12)
+        {
+            cout << "Redirecting to Childrens Section.... "<< endl;
+            //code for redirection
+        }
+        else if (age >=13 && age <=19)
+        {
+            cout << "Redirecting to Teen Section.... "<< endl;
+          
+        }
+        else if (age >=20 && age <=40)
+        {
+            cout << "Redirecting to Adult Section... "<< endl;
+        }
+        else if (age >=40 && age <=60)
+        {
+            cout << "Redirecting to Elder Section.... "<< endl;
+       
+        }
+        else
+        {
+            cout << "Technical Issues! Redirecting to home .... "<<endl;
+        }
+    
+
+    system("pause");
+    return 0;
+}
