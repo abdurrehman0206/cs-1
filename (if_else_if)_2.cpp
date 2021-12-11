cdc6882d2b4285fe3a3dd1fc64ea87edf582280d
abdurrehman0206 <68471828+abdurrehman0206@users.noreply.github.com>
2021-12-11 14:39:15 +0500

    (if_else_if)_2.:

---
+++ (if_else_if)_2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    char sign;
+    cout << "Enter 1st Number :: ";
+    cin >> a;
+    cout << "Enter 2nd Number :: ";
+    cin >> b;
+    cout << "Operation [+,-,*,/] :: ";
+    cin >> sign;
+
+    if (sign == '+' || sign == '-' || sign == '*' || sign == '/')
+    {
+        if (sign == '+')
+        {
+            cout << "Sum :: " << a + b << endl;
+        }
+        else if (sign == '-')
+        {
+            cout << "Difference [a-b] :: " << a - b << endl;
+            cout << "Difference [b-a] :: " << b - a << endl;
+        }
+        else if (sign == '*')
+        {
+            cout << "Product :: " << a * b << endl;
+        }
+        else if (sign == '/')
+        {
+            cout << "Division [a/b] :: " << a / b << endl;
+            cout << "Division [b/a] :: " << b / a << endl;
+        }
+        else
+        {
+            cout << "Technical Issues!"<<endl;
+        }
+    }
+    else
+    {
+        cout << "Enter Valid Operation!" << endl;
+    }
+
+    system("pause");
+    return 0;
+}
