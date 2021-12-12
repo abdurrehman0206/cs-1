4a24a2deb6880c49aa72d86dc2cf1e5758acbbac
abdurrehman0206 <68471828+abdurrehman0206@users.noreply.github.com>
2021-12-12 12:43:42 +0500

    .

--- PFL/Assignment-1/if_else_if/(if_else_if)_6.cpp
+++ PFL/Assignment-1/if_else_if/(if_else_if)_6.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main (){
+    
+    float num;
+    cout << "Enter a number :: ";
+    cin >> num;
+
+    if (num > 0){
+        cout << "Postive number !"<<endl;
+    }else if(num < 0){
+        cout << "Negative number !"<<endl;
+    }else{
+        cout << "Zero !"<<endl;
+    }
 
     system("pause");
     return 0;
