47b7921e7aab2e61b99d844d3087e179e40c8326
abdurrehman0206 <68471828+abdurrehman0206@users.noreply.github.com>
2021-12-12 14:00:11 +0500

    .

--- PFL/Assignment-1/if_else_if/(if_else_if)_10.cpp
+++ PFL/Assignment-1/if_else_if/(if_else_if)_10.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main (){
+int main()
+{
+
+    float pay;
+    cout << "Enter amount to be payed in $ :: ";
+    cin >> pay;
+
+    if (pay <= 200)
+    {
+        cout << "No Discount!" << endl;
+        cout << "To Be Paid :: " << pay << "$" << endl;
+    }
+    else if (pay > 200 && pay <= 400)
+    {
+        cout << "Discount Applied! 10% off " << endl;
+        cout << "Discount :: " << ((pay * 10) / 100) << endl;
+        cout << "To Be Paid :: " << pay - ((pay * 10) / 100) << endl;
+    }
+    else if (pay > 400 && pay <= 600)
+    {
+        cout << "Discount Applied! 15% off " << endl;
+        cout << "Discount :: " << ((pay * 15) / 100) << endl;
+        cout << "To Be Paid :: " << pay - ((pay * 15) / 100) << endl;
+    }
+    else if (pay > 600)
+    {
+        cout << "Discount Applied! 20% off " << endl;
+        cout << "Discount :: " << ((pay * 20) / 100) << endl;
+        cout << "To Be Paid :: " << pay - ((pay * 20) / 100) << endl;
+    }
+    else
+    {
+        cout << "Please enter valid amount!" << endl;
+    }
 
     system("pause");
     return 0;
