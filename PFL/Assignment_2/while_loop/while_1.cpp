38f375d21fa930f63e0e4d3b0140229298ae023d
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2022-01-16 22:17:55 +0500

    Update

--- PFL/Assignment_2/while_loop/while_1.cpp
+++ PFL/Assignment_2/while_loop/while_1.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
-    
-
-    
+int main()
+{
+    int num, rev = 0;
+    int tnum;
+    cout << "Input a number to reverse :: ";
+    cin >> num;
+    tnum = num;
+    cout << "Number :: " << num << endl;
+    while (num)
+    {
+        rev = rev * 10 + num % 10;
+        num = num / 10;
+    }
+    cout << "Reverse :: " << rev << endl;
+    if (tnum == rev)
+    {
+        cout << "Its a palindrome!" << endl;
+    }
+    else
+    {
+        cout << "Its not a palindrome!" << endl;
+    }
     system("pause");
     return 0;
 }
