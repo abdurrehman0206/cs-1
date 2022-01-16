820e70e90671e551b53b5551bccc6964dfe012b0
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2022-01-16 20:51:14 +0500

    Update

--- PFL/Assignment_2/nested_loop/nest_4.cpp
+++ PFL/Assignment_2/nested_loop/nest_4.cpp
@@ -2,15 +2,35 @@
 
 using namespace std;
 
-int main(){
-    int num;
-    cout << "Enter a Number :: ";
-    cin >> num;
+int main()
+{
 
-    for (int i = 1; ){
-        
+    unsigned int row, col;
+    cout << "Enter Number of Rows :: ";
+    cin >> row;
+    cout << "Enter Number of Columns :: ";
+    cin >> col;
+    int arr[row][col];
+    cout << row << "x" << col << " Matrix Created!" << endl;
+    cout << "_____INPUT_____" << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << "Input " << i << "x" << j << " :: ";
+            cin >> arr[i][j];
+        }
+    }
+    cout << "_____OUTPUT_____" << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << "  ";
+        }
+        cout << endl;
     }
 
-        system("pause");
+    system("pause");
     return 0;
 }
--- PFL/Assignment_2/nested_loop/nest_5.cpp
+++ PFL/Assignment_2/nested_loop/nest_5.cpp
@@ -2,10 +2,23 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
+    int range;
+    cout << "Enter Range :: ";
+    cin >> range;
 
+    for (int i = 1; i <= range; i++)
+    {
+        for (int j = 1; j <= i; j++){
+            for (int k = 1; k <= j;k++){
+                cout << j;
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
 
-    
     system("pause");
     return 0;
 }
