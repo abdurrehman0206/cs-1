06c6856926763836ab36691e624fece28dfd4213
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2022-01-16 19:12:36 +0500

    Update

--- PFL/Assignment-2/Pattern/Pattern_6.cpp
+++ PFL/Assignment-2/Pattern/Pattern_6.cpp
@@ -7,30 +7,24 @@ int main()
     int range, i, j, end, start;
     cout << "Enter range :: ";
     cin >> range;
-    start = range;
-    end = range;
+    start = 1;
+    end = (range * 2);
     for (i = 1; i <= range; i++)
     {
         for (j = 1; j < range * 2; j++)
         {
-            if (i != range)
-            {
-                if (j == end || j == start)
-                {
-                    cout << "*";
-                }
-                else
-                {
-                    cout << " ";
-                }
-            }
-            else
+
+            if (j >= start && j < end)
             {
                 cout << "*";
             }
+            else
+            {
+                cout << " ";
+            }
         }
-        end++;
-        start--;
+        end--;
+        start++;
         cout << endl;
     }
 
