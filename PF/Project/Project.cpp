abb58c2e7500d7edc145fc1e4f178fc202f8e7ee
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2022-01-21 16:47:08 +0500

    Update

--- PF/Project/Project.cpp
+++ PF/Project/Project.cpp
@@ -2,16 +2,38 @@
 
 int main()
 {
-    short n;
-    cout << "Input Number of Courses :: ";
-    cin >> n;
+    bool chk[5][6];
+    nullify(chk);
+    short n=6;
+    //cout << "Input Number of Courses :: ";
+    //cin >> n;
     system("CLS");
     Data ct[n];
+    Table tt[5][6];
     //d_input(ct,&n);
     
     //o_file(ct, &n);
     i_file(ct, &n);
-    d_output(ct,&n);
+    //d_output(ct,&n);
+    gen_Table(ct, tt, chk, &n);
+    add_Break(tt);
+    t_output(tt);
+    cout << endl
+         << endl;
+    randomize_table(tt);
+    t_output(tt);
+    cout << endl
+         << endl;
+    randomize_table(tt);
+    t_output(tt);
+    cout << endl
+         << endl;
+    randomize_table(tt);
+    t_output(tt);
+    cout << endl
+         << endl;
+    randomize_table(tt);
+    t_output(tt);
     system("pause");
     return 0;
 }
--- PF/Project/functions.h
+++ PF/Project/functions.h
@@ -10,8 +10,24 @@ struct Data
     short cr_h;
 };
 
+struct Table
+{
+    string block;
+};
+
 ifstream fin;
 ofstream fout;
+
+void nullify(bool chk[5][6])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            chk[i][j] = 0;
+        }
+    }
+}
 
 void d_input(Data *ct, short *n)
 {
@@ -66,5 +82,78 @@ void i_file(Data *ct, short *n)
         fin >> ct[i].t_name;
         fin >> ct[i].c_name;
         fin >> ct[i].cr_h;
+    }
+}
+
+void gen_Table(Data *ct, Table tt[5][6], bool chk[5][6], short *n)
+{
+    fout.open("table.txt", ios::out);
+    int c = 0;
+    int cnt = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (c > 5)
+        {
+            break;
+        }
+        for (int j = 0; j < 6; j++)
+        {
+            if (cnt < ct[c].cr_h)
+            {
+                tt[i][j].block = ct[c].c_name + "|" + ct[c].t_name;
+                cnt++;
+            }
+            else if (cnt >= ct[c].cr_h)
+            {
+                c++;
+                if (c > 5)
+                {
+                    break;
+                }
+                cnt = 0;
+            }
+        }
+    }
+}
+void add_Break(Table tt[5][6])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            if (tt[i][j].block == "")
+            {
+                tt[i][j].block = "Tutorial";
+            }
+        }
+    }
+}
+
+void t_output(Table tt[5][6])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            cout << setw(17) << left << tt[i][j].block;
+        }
+        cout << endl;
+    }
+}
+
+void randomize_table(Table tt[5][6])
+{
+    int rn;
+    string temp;
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            rn = (rand() % 4) + 1;
+            temp = tt[j][i].block;
+            tt[j][i].block = tt[rn][i].block;
+            tt[rn][i].block = temp;
+        }
+        cout << endl;
     }
 }
