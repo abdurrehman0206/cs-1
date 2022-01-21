0b5eea4bf62b848256d4d8db0f23187a993d584a
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2022-01-21 14:39:03 +0500

    Update

--- PF/Project/Project.cpp
+++ PF/Project/Project.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
 #include "functions.h"
-using namespace std;
-
-
 
 int main()
 {
@@ -11,9 +7,11 @@ int main()
     cin >> n;
     system("CLS");
     Data ct[n];
-    d_input(ct,&n);
+    //d_input(ct,&n);
+    
+    //o_file(ct, &n);
+    i_file(ct, &n);
     d_output(ct,&n);
-
     system("pause");
     return 0;
 }
--- PF/Project/functions.h
+++ PF/Project/functions.h
@@ -10,9 +10,12 @@ struct Data
     short cr_h;
 };
 
+ifstream fin;
+ofstream fout;
+
 void d_input(Data *ct, short *n)
 {
-    
+
     cout << "Input ::" << endl;
     cout << endl;
     cin.ignore();
@@ -34,7 +37,7 @@ void d_input(Data *ct, short *n)
 void d_output(Data *ct, short *n)
 {
     cout << "Teacher \t Course \t C.Hrs" << endl;
- 
+
     cout << endl;
     for (int i = 0; i < *n; i++)
     {
@@ -42,4 +45,26 @@ void d_output(Data *ct, short *n)
     }
     system("pause");
     system("CLS");
+}
+
+void o_file(Data *ct, short *n)
+{
+    fout.open("data.txt", ios::ate | ios::app);
+    for (int i = 0; i < *n; i++)
+    {
+        fout << ct[i].t_name << "\t" << ct[i].c_name << "\t" << ct[i].cr_h << endl;
+    }
+    fout.close();
+}
+
+void i_file(Data *ct, short *n)
+{
+    fin.open("data.txt", ios::in);
+    for (int i = 0; i < *n; i++)
+    {
+        //getline(fin, ct[i].t_name);
+        fin >> ct[i].t_name;
+        fin >> ct[i].c_name;
+        fin >> ct[i].cr_h;
+    }
 }
