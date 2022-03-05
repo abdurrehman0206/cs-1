5c6e708aacba5c4c6926e01c14b1d4b06599aa85
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2022-03-05 17:02:27 +0500

    Pointer Array Sort

---
+++ Manual/PointerArraySort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {5, 7, 4, 12, 78, 11, 0, 1, 9, 181, 171, 18};
+    int *current = arr;
+    int *next = &arr[1];
+    int temp;
+
+    cout << "Before Sorting :: ";
+    for (int i = 0; i < 12; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 1; i < 12; i++)
+    {
+        for (int j = i - 1; j < 12; j++)
+        {
+            if (*current > *next)
+            {
+                temp = *current;
+                *current = *next;
+                *next = temp;
+            }
+            *next++;
+        }
+        *current++;
+        next = current;
+    }
+
+    cout << "After Sorting :: ";
+    for (int i = 0; i < 12; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    system("pause");
+    return 0;
+}
