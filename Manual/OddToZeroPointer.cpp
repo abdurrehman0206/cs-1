0b21e12d9a60e60f2d88b9cf8fe048e23747b208
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2022-03-06 13:06:37 +0500

    Odd Number to zero using pointer

---
+++ Manual/OddToZeroPointer.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int *isOdd(int *p)
+{
+
+    for (int i = 0; i < 21; i++)
+    {
+
+        if (*(p) % 2 != 0)
+        {
+            *(p) = 0;
+        }
+        p++;
+    }
+
+    return p;
+}
+
+int main()
+{
+
+    int a[] = {24, 3489, 14, 1548, 18, 154, 3465, 164, 1233, 156, 186, 164, 15, 164, 1233, 156, 446, 17, 9, 3, 15};
+    int *p;
+
+    p = isOdd(a);
+    for (int i = 0; i < 21; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    system("pause");
+    return 0;
+}
