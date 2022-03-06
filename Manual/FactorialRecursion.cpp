e774470c270c8b5cff74d7899e578c5bf32c8c59
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2022-03-06 15:05:42 +0500

    Recursion Factorial

---
+++ Manual/FactorialRecursion.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int f)
+{
+    if (f == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return f * factorial(f - 1);
+    }
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a Number :: ";
+    cin >> num;
+
+    cout << "Factorial :: " << factorial(num) << endl;
+
+    system("pause");
+    return 0;
+}
