046d4ee06b1677580b9611a7d9e0465c0acd3e14
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2022-03-06 13:31:21 +0500

    Pointer to delete an element in an array

---
+++ Manual/ArrDeletePointer.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int *del(int *p)
+{
+    int ind, temp;
+    cout << "Enter Index to delete :: ";
+    cin >> ind;
+    int *current = p, *next = p;
+    current += ind;
+    *current = 0;
+    next += ind;
+    next++;
+    for (int i = ind; i < 20; i++)
+    {
+        temp = *current;
+        *current = *next;
+        *next = temp;
+        current++;
+        next++;
+    }
+    return p;
+}
+
+int main()
+{
+    int a[] = {24, 3489, 14, 1548, 18, 154, 3465, 164, 1233, 156, 186, 164, 15, 164, 1233, 156, 446, 17, 9, 3, 15};
+    int *p;
+    cout << "Before Delete :: " << endl;
+    for (int i = 0; i < 21; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    p = del(a);
+    cout << "After Delete :: " << endl;
+    for (int i = 0; i < 21; i++)
+    {
+        cout << a[i] << " ";
+    }
+    system("pause");
+    return 0;
+}
